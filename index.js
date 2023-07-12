934cda345aed0901a93b3f4c864d96159acf7d10
AntonYakavenka <doomanton@gmail.com>
2023-07-12 14:57:08 +0300

    hw 1 update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// -------------------- 02 ---------------------------
+
 var number1 = Math.floor(Math.random() * 100)
 var remainder = number1 % 2
 
@@ -6,3 +8,51 @@ if (remainder === 0) {
 } else {
   console.log(number1, 'Это нечетное число')
 }
+
+// -------------------- 03 ---------------------------
+
+let number = 1;
+
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
+
+// -------------------- 04 ---------------------------
+
+let x = 5;
+x += 3;
+x *= 2;
+x -= 4;
+x /= 3;
+
+console.log(x);
+
+// -------------------- 05 ---------------------------
+
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+
+switch (true) {
+  case rule.length < 25:
+    console.log("Все таки нет правил без исключения");
+    break;
+  case rule.length > 25:
+    console.log("У меня 100 пудов все получится");
+    break;
+  default:
+    console.log("50 на 50");
+    break;
+}
+
+// -------------------- 06 ---------------------------
+
+let str = "zzzzyzz";
+let result = str.includes("y") ? "нашел" : "заблудился";
+console.log(result);
+
+// -------------------- 07 ---------------------------
+
+let iAm = 'я'
+let krasavchik = "красавчик!";
+
+console.log(iAm[0].toUpperCase(), krasavchik);
