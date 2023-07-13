8306e2b11e6add2644c711b028164ea153460a0a
AntonYakavenka <doomanton@gmail.com>
2023-07-13 11:19:12 +0300

    add files

---
+++ 01-02.js
@@ -0,0 +1,8 @@
+var number1 = Math.floor(Math.random() * 100)
+var remainder = number1 % 2
+
+if (remainder === 0) {
+  console.log(number1, 'Это четное число')
+} else {
+  console.log(number1, 'Это нечетное число')
+}
