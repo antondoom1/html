7c19fba6fd07124a647b1cd312c08fba577a9c73
AntonYakavenka <doomanton@gmail.com>
2023-07-26 13:07:12 +0300

    add lesson 3 tasks

---
+++ lesson-03/03.js
@@ -0,0 +1,5 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+const newFruits = fruits.slice(1, -2);
+
+console.log(newFruits);
