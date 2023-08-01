ea6b599009029c27a76bfe10775a6b542db99c52
AntonYakavenka <doomanton@gmail.com>
2023-08-01 12:25:14 +0300

    add lesson 4

---
+++ index.js
@@ -0,0 +1,20 @@
+let timerId; // переменная, которая будет хранить ID таймера
+
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', function () {
+    timerId = setInterval(updateClock, 1000); // запускаем таймер, который будет вызывать функцию updateClock каждую секунду
+});
+
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+    clearInterval(timerId); // останавливаем таймер
+});
+
+function updateClock() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
+    clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
